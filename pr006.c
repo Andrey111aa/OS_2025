002293cb293aebdefc132141dc9b724913c27f4c
avdeev <avdeev-and2007@mail.ru>
2025-04-21 15:25:50 +0700

    Добавили вывод значений переменных среды, добавили условие изменения поведения программы

--- pr006.c
+++ pr006.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 
 int main (int argc, char* argv[], char* envp[]) {
-	printf ("Количество аргументов, переданных порограмме: %d\n", argc);
-	printf("\n");
-	for (int i=0; i < argc; i++)
+	if (argc >=2) {
+ 		printf ("Количество аргументов, переданных порограмме: %d\n", argc);
+		printf("\n");
+		for (int i=0; i < argc; i++)
 		printf("Аргумент[%d]: %s\n", i, argv[i]);
-	printf("\n");
+		printf("\n");
+	}
+
+	else {
+		printf("Список переменных среды, в которых запущена программа:\n");
+	int j=0;
+
+	while (envp[j]!=NULL) {
+		printf("%d %s\n", j, envp[j]);
+		j++;
+		}
+	}	
 return 0;
 }
